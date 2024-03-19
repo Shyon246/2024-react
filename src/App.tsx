e76163704f3231fcd08d012e33110e89a30692b6
Milton Shyon <94058366+Shyon246@users.noreply.github.com>
2024-03-19 17:03:36 -0300

    Voltando o código para o inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import "./App.css";
-
-const meubotao = (props: any) => {
-  return <button>{props.titulo}</button>;
+const App = () => {
+  return (
+    <div>
+      <h1>Bem vindo ao mundo React</h1>
+      <button>eu sou um botão html</button>
+    </div>
+  );
 }
 
-const App = () => {
-    return (
-      <div className="aplicacao">
-        <h1>Bem vindo ao mundo React</h1>
-        <MeuBotao titulo="m1"/>
-        <MeuBotao titulo="m2"/>
-        <MeuBotao titulo="m3"/>
-     </div>
-    );
-  }
-  
-  export default App;
-  
+export default App;
